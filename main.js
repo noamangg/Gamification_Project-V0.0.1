4a1a12e3e81c034cb55e5afd6de8efb67c13dc5a
Noaman Grewaity <noamangg@gmail.com>
2023-01-19 02:50:22 +0300

    Days 4 and make the array

--- main.js
+++ main.js
@@ -36,23 +36,32 @@ function countUp() {
   secondsBox.textContent = addZeroes(secondsEnd);
 }
 function monthValue(month) {
-  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  for(let i = 0; i < months.length; i++)
-  {
-    if(month === i)
-    return months[i][0] + months[i][1] + months[i][2];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  for (let i = 0; i < months.length; i++) {
+    if (month === i) return months[i][0] + months[i][1] + months[i][2];
   }
   return months[0][0] + months[0][1] + months[0][2];
 }
 let addZeroes = (num) => (num < 10 ? `0${num}` : num);
-
 
 // Counter
 let dayBox = document.getElementById("day-box");
 let hoursBox = document.getElementById("hr-box");
 let minutesBox = document.getElementById("mi-box");
 let secondsBox = document.getElementById("se-box");
-
 
 let startDate = new Date(2023, 0, 15, 5, 0);
 let startTime = startDate.getTime();
@@ -62,9 +71,13 @@ let i = setInterval(countUp, Number.MAX_VALUE);
 
 countUp();
 
-document.getElementById('year-screen').innerHTML = startDate.getFullYear();
-document.getElementById('month-screen').innerHTML = monthValue(startDate.getMonth());
-document.getElementById('day-screen').innerHTML = addZeroes(startDate.getDate());
+document.getElementById("year-screen").innerHTML = startDate.getFullYear();
+document.getElementById("month-screen").innerHTML = monthValue(
+  startDate.getMonth()
+);
+document.getElementById("day-screen").innerHTML = addZeroes(
+  startDate.getDate()
+);
 
 // Checklist
 let timeChecked = [
@@ -83,12 +96,45 @@ let timeChecked = [
 checkItems();
 fillBG();
 
+// function fillChecked() {
+//   let j = document.getElementById('progress-bar');
+//   for(let i = 0; i < 25; i++)
+//   j.document.write("Hellowdsaf");
+// }
+// fillChecked();
+
+// Add Some Days
+
+let days = [];
+days.push(`I deleted the YouTube app and the Chrome browser on my phone, I was so tired and I
+had an exam in the university so I didn't think of dopamine.`);
+
+days.push(`In this day, I was feeling so badly that because I need some dopamine literally I
+was locking for anything can give this feeling but fortunately there is nothing so I move on.`);
+
+days.push(`I was so upsets I didn't get enough sleep and I felt like I really need some cool
+dopamine stuff for that I reach to my default browser on my phone and I open it then went to YouTube I
+(froze) there because on more step and It will break the challenge so I told to myself let's learn some
+CSS instead so I start watch some CSS videos and learn some stuffs then when I finished I close the
+browser and I will not open it again, after that I opened my phone and start reading some PDF books,
+it's work and I forgat dopamine.`);
+
+days.push(`No more dopamine today I was so enjoy doing sport I had a great sport's
+section, I didn't get enough sleep but it's ok I did very well in my today's exam, but I spent mush time
+jumping from work group to another in the chat app so it's problem that I should solve.`);
 
 
-function fillChecked() {
-  let j = document.getElementById('progress-bar');
-  for(let i = 0; i < 25; i++)
-  j.document.write("Hellowdsaf");
+let daysSave = ``;
+for (let i = 0; i < days.length; i++) {
+  daysSave += `
+  <div class=days-card>
+    <div class="days-front">
+      <p><span>Day ${i + 1}:</span> ${days[i]}</p>
+    </div>
+    <div class="days-back">
+      <p>Day ${i + 1}<p/>
+    </div>
+  </div>
+  `;
 }
-fillChecked();
-
+document.getElementById("journalist-section").innerHTML = daysSave;
